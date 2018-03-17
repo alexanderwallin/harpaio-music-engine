1babd4c96886f581e2aac3161744f3b27e0f2f71
Alexander Wallin <office@alexanderwallin.com>
2018-03-17 19:04:59 +0000

    Captures controllers 0-127 in map-controller.js

--- src/setup/map-controller.js
+++ src/setup/map-controller.js
@@ -5,7 +5,7 @@ const { relayCc } = require('../cc-relays.js')
 async function run() {
   relayCc({
     channels: range(0, 15),
-    controlIds: range(0, 15),
+    controlIds: range(0, 127),
     device: `IAC Driver Bus 1`,
     onRelay: packet => console.log(packet),
   })
